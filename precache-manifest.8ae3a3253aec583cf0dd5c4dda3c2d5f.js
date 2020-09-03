ceebc453f2b3054eab48c3e9a62f4ea5dce6ed66
shubhamkumbhakar <shubhamkumbhakar27@gmail.com>
2020-09-03 13:54:22 +0530

    Updates

---
+++ precache-manifest.8ae3a3253aec583cf0dd5c4dda3c2d5f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ccf4377d717ec21658f1b32f68d4344",
+    "url": "/pretva-assign/index.html"
+  },
+  {
+    "revision": "3b778ce68f51c559e3c6",
+    "url": "/pretva-assign/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "d8777815cba2d40a5fb8",
+    "url": "/pretva-assign/static/css/main.221bd6bf.chunk.css"
+  },
+  {
+    "revision": "3b778ce68f51c559e3c6",
+    "url": "/pretva-assign/static/js/2.dc3580c5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pretva-assign/static/js/2.dc3580c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8777815cba2d40a5fb8",
+    "url": "/pretva-assign/static/js/main.9b3bf374.chunk.js"
+  },
+  {
+    "revision": "32e0ab74578b16074234",
+    "url": "/pretva-assign/static/js/runtime-main.92f9e51f.js"
+  },
+  {
+    "revision": "de0d46d9aed0f3bc5a627b24240f079e",
+    "url": "/pretva-assign/static/media/Image1.de0d46d9.png"
+  },
+  {
+    "revision": "d7407a29a1393ea9d6042769b0bcc0c3",
+    "url": "/pretva-assign/static/media/image2.d7407a29.png"
+  },
+  {
+    "revision": "d3e0c1d0a290386f60d36ebd3942ee38",
+    "url": "/pretva-assign/static/media/image3.d3e0c1d0.png"
+  },
+  {
+    "revision": "df14962df734023d6f9599d71e7ff5c7",
+    "url": "/pretva-assign/static/media/logo.df14962d.png"
+  }
+]);
